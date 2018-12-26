d378eb5689a1d74f50621c04699476c1372d7d8e
benpaodewuhuarou <zywang1218@gmail.com>
2018-12-26 15:58:02 -0500

    Ingtegrated nested attributes for potfolio model

--- app/models/porfolio.rb
+++ app/models/porfolio.rb
@@ -1,5 +1,7 @@
 class Porfolio < ApplicationRecord
 	has_many :technologies, dependent: :destroy
+
+	accepts_nested_attributes_for :technologies, reject_if: lambda{|attrs| attrs['name'].blank?}
 	include Placeholder
 	validates_presence_of :title, :body, :main_image, :thumb_image
 
