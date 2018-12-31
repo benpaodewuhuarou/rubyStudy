a23a2084c4e8b831de86458a5b019bd96954b357
benpaodewuhuarou <zywang1218@gmail.com>
2018-12-31 15:17:47 -0500

    Installed petergate authorization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,5 +5,18 @@ class ApplicationController < ActionController::Base
 	include SetSource
 	include CurrentUserConcern
 	include DefaultPageContent
+	before_action :set_copyright
 
+	def set_copyright
+		@copyright = DevcamoViewTool::Renderer.copyright 'zhiyang wang', 
+		'All rights reserved'
+	end
+end
+
+module DevcamoViewTool
+	class Renderer
+		def self.copyright name, msg
+			"&copy; #{Time.now.year} | <b> #{name} </b> #{msg}".html_safe
+		end
+	end
 end
